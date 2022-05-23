c3f891d3079fc634d5bf0e95f7f8af9954f8dd0d
mdsayeedsalam2 <106056453+mdsayeedsalam2@users.noreply.github.com>
2022-05-23 21:27:02 +0600

    Update tictactoe.py
    Added from in comment

--- tictactoe.py
+++ tictactoe.py
@@ -3,7 +3,7 @@
 # There will be two players in a game. Two signs X and O represent each player. 
 # This program supports variable sized TicTacToe board, e.g. 3x3 / 5x5 / 7x7 / 100x100 board size
 # It works for board sizes 3x3 to 4x4 as upper_size = 4 is set.
-# Just change the value of upper_size = 7 to work for board sizes 3x3 to 7x7 etc.
+# Just change the value of upper_size = 7 to work for board sizes from 3x3 to 7x7 etc.
 # The game is automatically played by the program and hence no user input is needed
 
 import random
