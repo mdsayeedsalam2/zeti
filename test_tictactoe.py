9404de183ce495ba6bc82653daaa8d5081e6efc9
Sayeed Salam <106056453+mdsayeedsalam2@users.noreply.github.com>
2022-05-29 19:27:43 +0600

    Update test_tictactoe.py
    This unit test works for the updated code that does not use native Python loops

--- test_tictactoe.py
+++ test_tictactoe.py
@@ -1,5 +1,6 @@
 # File name: test_tictactoe.py
 # Contains Python unit tests code for tic tac toe game code shown in tictactoe.py file
+import numpy as np
 import unittest
 from tictactoe import TicTacToe
 
@@ -8,34 +9,39 @@ class TestTicTacToe(unittest.TestCase):
 
 # checks whether creating variable sized empty TicTacToe board or not
     def test_create_variable_sized_empty_board(self):
-        self.assertEqual(TicTacToe.create_board(self, 4), [['-', '-', '-', '-'], ['-', '-', '-', '-'], ['-', '-', '-', '-'], ['-', '-', '-', '-']])
-        self.assertEqual(TicTacToe.create_board(self, 3), [['-', '-', '-'], ['-', '-', '-'], ['-', '-', '-']])
+        self.assertSequenceEqual(TicTacToe.create_board(self, 4).tolist(), [['-', '-', '-', '-'], ['-', '-', '-', '-'], ['-', '-', '-', '-'], ['-', '-', '-', '-']])
+        self.assertSequenceEqual(TicTacToe.create_board(self, 3).tolist(), [['-', '-', '-'], ['-', '-', '-'], ['-', '-', '-']])
 
 
 # checks whether determining row-wise win or not
     def test_row_wise_win(self):
-        self.assertTrue(TicTacToe.does_player_win(self, 'X', [['X', 'X', 'X'], ['-', '-', '-'], ['-', '-', '-']]))
-        self.assertFalse(TicTacToe.does_player_win(self, 'O', [['X', 'X', 'X'], ['-', '-', '-'], ['-', '-', '-']]))
+        self.assertTrue(TicTacToe.does_player_win(self, 'X', np.array([['X', 'X', 'X'], ['-', '-', '-'], ['-', '-', '-']])))
+        self.assertFalse(TicTacToe.does_player_win(self, 'O', np.array([['X', 'X', 'X'], ['-', '-', '-'], ['-', '-', '-']])))
+
 
 # checks whether determining column-wise win or not
     def test_column_wise_win(self):
-        self.assertTrue(TicTacToe.does_player_win(self, 'O', [['O', '-', '-'], ['O', '-', '-'], ['O', '-', '-']]))
-        self.assertFalse(TicTacToe.does_player_win(self, 'X', [['O', '-', '-'], ['O', '-', '-'], ['O', '-', '-']]))
+        self.assertTrue(TicTacToe.does_player_win(self, 'O', np.array([['O', '-', '-'], ['O', '-', '-'], ['O', '-', '-']])))
+        self.assertFalse(TicTacToe.does_player_win(self, 'X', np.array([['O', '-', '-'], ['O', '-', '-'], ['O', '-', '-']])))
+
 
 # checks whether determining 1st diagonally(top-left to bottom-right) win or not
     def test_1st_diagonal_win(self):
-        self.assertTrue(TicTacToe.does_player_win(self, 'O', [['O', '-', '-'], ['-', 'O', '-'], ['-', '-', 'O']]))
-        self.assertFalse(TicTacToe.does_player_win(self, 'X', [['O', '-', '-'], ['-', 'O', '-'], ['-', '-', 'O']]))
+        self.assertTrue(TicTacToe.does_player_win(self, 'O', np.array([['O', '-', '-'], ['-', 'O', '-'], ['-', '-', 'O']])))
+        self.assertFalse(TicTacToe.does_player_win(self, 'X', np.array([['O', '-', '-'], ['-', 'O', '-'], ['-', '-', 'O']])))
  
+
 # checks whether determining 2nd diagonally(bottom-left to top-right) win or not
     def test_2nd_diagonal_win(self):
-        self.assertTrue(TicTacToe.does_player_win(self, 'X', [['-', '-', 'X'], ['-', 'X', '-'], ['X', '-', '-']]))
-        self.assertFalse(TicTacToe.does_player_win(self, 'O', [['-', '-', 'X'], ['-', 'X', '-'], ['X', '-', '-']]))
+        self.assertTrue(TicTacToe.does_player_win(self, 'X', np.array([['-', '-', 'X'], ['-', 'X', '-'], ['X', '-', '-']])))
+        self.assertFalse(TicTacToe.does_player_win(self, 'O', np.array([['-', '-', 'X'], ['-', 'X', '-'], ['X', '-', '-']])))
+
 
 # checks whether the TicTacToe board is filled or not
     def test_filled(self):
-        self.assertTrue(TicTacToe.is_filled(self, [['O', 'O', 'X'], ['X', 'X', 'O'], ['O', 'X', 'O']]))
-        self.assertFalse(TicTacToe.is_filled(self, [['O', 'O', 'X'], ['X', '-', 'O'], ['O', 'X', 'O']]))
+        self.assertTrue(TicTacToe.is_filled(self, np.array([['O', 'O', 'X'], ['X', 'X', 'O'], ['O', 'X', 'O']])))
+        self.assertFalse(TicTacToe.is_filled(self, np.array([['O', 'O', 'X'], ['X', '-', 'O'], ['O', 'X', 'O']])))
+
 
 # checks whether swap player for the next turn or not
     def test_player_swap(self):
@@ -45,6 +51,7 @@ class TestTicTacToe(unittest.TestCase):
 
 # checks for empty places on TicTacToe board
     def test_empty_places(self):
-        self.assertEqual(TicTacToe.empty_places(self, [['O', 'O', 'X'], ['X', 'X', 'O'], ['O', 'X', 'O']]), [])
-        self.assertEqual(TicTacToe.empty_places(self, [['O', '-', 'X'], ['X', 'X', 'O'], ['O', 'X', 'O']]), [(0,1)])
-        self.assertEqual(TicTacToe.empty_places(self, [['O', '-', 'X'], ['-', 'X', 'O'], ['O', 'X', '-']]), [(0,1),(1,0),(2,2)])
+        self.assertEqual(np.array(TicTacToe.empty_places(self, np.array([['O', 'O', 'X'], ['X', 'X', 'O'], ['O', 'X', 'O']]))).tolist(), [[],[]])
+        self.assertTrue(np.array(TicTacToe.empty_places(self, np.array([['O', '-', 'X'], ['X', 'X', 'O'], ['O', 'X', 'O']]))).tolist()== [[0],[1]])
+        self.assertEqual(np.array(TicTacToe.empty_places(self, np.array([['O', '-', 'X'], ['-', 'X', 'O'], ['O', 'X', '-']]))).tolist(), [[0, 1, 2], [1, 0, 2]])
+ 
